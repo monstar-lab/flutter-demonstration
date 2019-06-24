9357c444fb7a6273cb775e65ced0bc431bdefc0c
ebiryu <ebiryu.zatta@gmail.com>
2019-06-24 17:26:18 +0900

    add images

---
+++ lib/utils/assets.dart
@@ -0,0 +1,2 @@
+const ASSET_FOX_IMAGE_HALF = 'assets/items/fox_half.jpg';
+const ASSET_FOX_IMAGE_QUARTER = 'assets/items/fox_quarter.jpg';
--- lib/widgets/itemList.dart
+++ lib/widgets/itemList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_livecoding/utils/assets.dart' as Assets;
 
 class ItemList extends StatelessWidget {
   @override
@@ -7,6 +8,12 @@ class ItemList extends StatelessWidget {
       children: <Widget>[
         ListTile(
           title: Text('一つ目の写真'),
+          leading: Image.asset(
+            Assets.ASSET_FOX_IMAGE_QUARTER,
+            width: 100,
+            height: 100,
+            fit: BoxFit.cover,
+          ),
           onTap: () {},
         ),
       ],
